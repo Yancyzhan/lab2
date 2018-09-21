4d3f5ae0feea244a598d4106d21638312609d224
Yancyzhan <38118492+Yancyzhan@users.noreply.github.com>
2018-09-21 14:24:26 -0600

    1

---
+++ graphicsWorld.h
@@ -0,0 +1,12 @@
+// graphicsWorld.h
+// ENSF 480 - Lab2 - Exercise A
+
+// Author: Yanzhao Zhang 30031217
+//
+// Date: Sept 21, 2018
+
+class GraphicsWorld{
+	
+	public:
+	void GraphicWorld::run();
+}
